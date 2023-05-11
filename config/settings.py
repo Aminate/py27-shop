670c1a40881650944a71334d896f9c757213e6ad
Amina <aminuwko@gmail.com>
2023-05-11 10:56:35 +0600

    amazon

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = 'eFSGRDHTFGMJ12345Y6sfVCB'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split()
 
 
 # Application definition
